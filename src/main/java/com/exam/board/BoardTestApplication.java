3c2cb614a5ab7841262a69cfe8d00acf1c8a7f29
DeepSeeDev <hyuk2000s@naver.com>
2025-07-09 18:20:45 +0900

    war 7-9

--- src/main/java/com/exam/board/BoardTestApplication.java
+++ src/main/java/com/exam/board/BoardTestApplication.java
@@ -15,7 +15,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class BoardTestApplication extends SpringBootServletInitializer {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BoardTestApplication.class, args);
 	}
